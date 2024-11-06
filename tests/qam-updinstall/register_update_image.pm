76e7d9352030b1027fbd35f7f42bd8c8f4c4ca85
user322 <user322@linux.fritz.box>
2024-11-06 18:13:47 +0100

    Move back to my reg code and delete printer

--- tests/qam-updinstall/register_update_image.pm
+++ tests/qam-updinstall/register_update_image.pm
@@ -12,23 +12,9 @@ use base "opensusebasetest";
 use testapi;
 use serial_terminal 'select_serial_terminal';
 use version_utils qw(is_sles4sap is_sle);
-use suseconnect_register;
+use registration qw (get_addon_fullname);
 
-use Data::Printer;
 
-my %addon_name = (
-    'base' => 'sle-module-basesystem',
-    'contm' => 'sle-module-containers',
-    'ha' => 'sle-ha',
-    'lgm' => 'sle-module-legacy',
-    'pcm' => 'sle-module-public-cloud',
-    'phub' => 'PackageHub',
-    'python2' => 'sle-module-python2',
-    'python3' => 'sle-module-python3',
-    'sapapp' => 'sle-module-sap-applications',
-    'sdk' => 'sle-module-development-tools',
-    'serverapp' => 'sle-module-server-applications'
-);
 
 sub get_included_products
 {
@@ -39,8 +25,8 @@ sub get_included_products
     elsif (is_sle('=15-SP5') or is_sle('=15-SP6')) {
         return "base,serverapp,ha,sapapp,python3";    # Desktop applications intentionally left out.
     }
-    elsif (is_sle('=12-SP5')){
-        assert_script_run ('SUSEConnect -l');
+    elsif (is_sle('=12-SP5')) {
+        return "";
     }
     else {
         die "Code under construction!";
@@ -60,18 +46,15 @@ sub run {
     # Register the base product.
     assert_script_run("SUSEConnect -r $base_product_key");
 
-    suseconnect_registration;
-
-    # my $version_id = get_required_var('VERSION');
-    # $version_id =~ s/-SP/./;
-    # my $cpu = get_required_var('ARCH');
-    # my @unregistered_addons = split(',', get_required_var("SCC_ADDONS"));
-    # p @unregistered_addons;
-    # my @addons_to_register = grep { not(get_included_products =~ /$_/) } @unregistered_addons;
-    # p @addons_to_register;
-    # foreach my $addon (@addons_to_register) {
-    #     assert_script_run("SUSEConnect -p $addon_name{$addon}/$version_id/$cpu");
-    # }
+    my $version_id = get_required_var('VERSION');
+    $version_id =~ s/-SP/./;
+    my $cpu = get_required_var('ARCH');
+    my @unregistered_addons = split(',', get_required_var("SCC_ADDONS"));
+    my @addons_to_register = grep { not(get_included_products =~ /$_/) } @unregistered_addons;
+    foreach my $addon (@addons_to_register) {
+        my $addon_name = get_addon_fullname($addon);
+        assert_script_run("SUSEConnect -p $addon_name/$version_id/$cpu");
+    }
 }
 
 1;
