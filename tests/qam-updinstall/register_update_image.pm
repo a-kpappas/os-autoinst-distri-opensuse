d0aa1ad84d07193a3afa6fa888c71553531d9cdc
Antonios Pappas <apappas@suse.com>
2024-11-12 16:29:07 +0100

    Add comments and the desktop module

--- tests/qam-updinstall/register_update_image.pm
+++ tests/qam-updinstall/register_update_image.pm
@@ -14,14 +14,16 @@ use serial_terminal 'select_serial_terminal';
 use version_utils qw(is_sles4sap is_sle);
 use registration qw (get_addon_fullname);
 
+
+# Returns a string containing the addons that are added to the system when the base product is registered
 sub get_included_products
 {
     die "Code under construction" if not is_sles4sap;
     if (is_sle('=15-SP2') or is_sle('=15-SP3') or is_sle('=15-SP4')) {
-        return "base,serverapp,ha,sapapp";
+        return "base,serverapp,ha,sapapp,desktop";
     }
     elsif (is_sle('=15-SP5') or is_sle('=15-SP6')) {
-        return "base,serverapp,ha,sapapp,python3";    # Desktop applications intentionally left out.
+        return "base,serverapp,ha,sapapp,python3,desktop";
     }
     elsif (is_sle('=12-SP5')) {
         return "";
@@ -33,41 +35,53 @@ sub get_included_products
 
 
 sub run {
+    select_serial_terminal;
+    # Register base product
+    #
+    #SLES(-HA) and SLES4SAP are different base products. Registering them using
+    #SUSEConnect only requires a product key.
+
     my $base_product_key;
+
     if (is_sles4sap) {
         $base_product_key = get_required_var("SCC_REGCODE_SLES4SAP");
     }
     else {
         $base_product_key = get_required_var("SCC_REGCODE");
     }
-
-
-    select_serial_terminal;
-
-    # Register the base product.
     assert_script_run("SUSEConnect -r $base_product_key");
 
+    # Register addons.
+
+    # Registering them requires inputing the CPU and VERSION_ID of the SUT. We
+    # are getting these from openQA vars and not /etc/os-release because it is
+    # easier.
     my $version = get_required_var('VERSION');
     my $version_id = $version;
     $version_id =~ s/-SP/./;
     my $cpu = get_required_var('ARCH');
 
+
     my $scc_addons = get_required_var('SCC_ADDONS');
 
-    if (is_sle '=12-SP5') {
+    # All products need LTSS for 12-SP5.
+    if (is_sle '=12-SP5'){
         my $ltss_key = get_required_var("SCC_REGCODE_LTSS");
         assert_script_run("SUSEConnect -p SLES-LTSS/12.5/$cpu -r $ltss_key");
         $scc_addons = s/ltss[,]?//;
     }
 
 
-    # Do not add PackageHub if the update package is qemu
+    # Do not add PackageHub if the update package is qemu. There is a known
+    # conflict and PackageHub is not supported.
     if (get_var('BUILD') =~ /qemu/ && get_var('INCIDENT_REPO') !~ /Packagehub-Subpackages/) {
         record_info('No PackageHub', 'known conflict on qemu update with phub repo poo#162704');
         $scc_addons = s/phub[,]?//;
     }
-
+    
     my @unregistered_addons = split(',', $scc_addons);
+
+    # Remove already registered addons from the list of addons to register to save on typing.
     my @addons_to_register = grep { not(get_included_products =~ /$_/) } @unregistered_addons;
     foreach my $addon (@addons_to_register) {
         my $addon_name = get_addon_fullname($addon);
