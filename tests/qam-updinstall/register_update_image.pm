69c058e257506ca2d59558beab64801ddb93feaf
Antonios Pappas <apappas@suse.com>
2024-12-06 16:45:51 +0100

    Handle SLE12 version_ids

--- tests/qam-updinstall/register_update_image.pm
+++ tests/qam-updinstall/register_update_image.pm
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: FSFAP
 
 # Summary: Bring up for the SAP/HA incident install tests in openQA
+#
+# This module re-implements a lot of the logic that is necessary to use
+# SUSEconnect because the appropriate library module, lib/suseconnect_register,
+# does not handle SLES4SAP correctly.
 
 use strict;
 use warnings;
@@ -15,7 +19,33 @@ use version_utils qw(is_sles4sap is_sle);
 use registration qw (get_addon_fullname);
 
 
-# Returns a string containing the addons that are added to the system when the base product is registered
+# SLE12 and SUSEConnect are inconsistent in the numbering scheme for the
+# different modules. Some need the VERSION_ID from /etc/os-release be supplied
+# when registered while some need the major version. The hash below provides the
+# mapping and was derived by running 'SUSEConnect -l' in a SLES4SAP 12-SP5
+# system.
+# For SLE15, SUSEConnect uses the VERSION_ID for all service packs.
+
+my %version_map = (
+    'sle-module-legacy' => '12',
+    'sle-module-web-scripting' => '12',
+    'sle-module-adv-systems-management' => '12',
+    'sle-module-public-cloud' => '12',
+    'sle-module-containers' => '12',
+    'sle-module-toolchain' => '12',
+    'sle-pos' => '12.2',
+    'sle-module-hpc' => '12',
+    'sle-ha-geo' => '12.5',
+    'sle-live-patching' => '12.5',
+    'sle-sdk' => '12.5',
+    'sle-we' => '12.5',
+    'PackageHub' => '12.5'
+);
+
+
+# Returns a string containing the addons that are added to the system when the
+# base product is registered. We use this function to avoid pinging SCC
+# unnecessarily.
 sub get_included_products
 {
     my $flavor = get_var("FLAVOR");
@@ -38,6 +68,8 @@ sub get_included_products
         die "This module might be scheduled for the wrong product";
     }
 }
+
+
 
 sub run {
     select_serial_terminal;
@@ -98,6 +130,7 @@ sub run {
     foreach my $addon (@addons_to_register) {
         my $addon_name = get_addon_fullname($addon);
         die "Invalid addon name. Check if SCC_ADDONS var is set correctly." unless (defined $addon_name);
+        $version_id = $version_map{$addon_name} if (is_sle('=12-SP5'));    # Version ids vary per addon in SLE12.
         assert_script_run("SUSEConnect -p $addon_name/$version_id/$cpu");
     }
 }
